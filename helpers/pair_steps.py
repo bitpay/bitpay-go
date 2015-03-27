13c75cba312c5c3f1e9fd982fe736e667bbf1ccb
Paul Daigle <paul@bitpay.com>
2015-03-27 00:28:55 -0400

    echo variables to look for duplicates

--- helpers/pair_steps.py
+++ helpers/pair_steps.py
@@ -15,7 +15,7 @@ def get_claim_code_from_server():
     browser.visit(ROOT_ADDRESS + "/merchant-login")
     browser.fill_form({"email": USER_NAME, "password": PASSWORD})
     browser.find_by_id("loginButton")[0].click()
-    time.sleep(1)
+    time.sleep(2)
     browser.visit(ROOT_ADDRESS + "/api-tokens")
     browser.find_by_css(".token-access-new-button").find_by_css(".btn").find_by_css(".icon-plus")[0].click()
     browser.find_by_id("token-new-form").find_by_css(".btn")[0].click()
