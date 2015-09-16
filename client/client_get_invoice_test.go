5a7b035eddeb12ad113f6496716790a1d47d159f
Paul Daigle <paul@bitpay.com>
2015-09-16 16:07:16 -0400

    temporary fix

--- client/client_get_invoice_test.go
+++ client/client_get_invoice_test.go
@@ -2,10 +2,9 @@ package client_test
 
 import (
 	. "github.com/bitpay/bitpay-go/client"
-	ku "github.com/bitpay/bitpay-go/key_utils"
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
-	"io/ioutil"
+	//"io/ioutil"
 	"os"
 	"time"
 )
@@ -13,31 +12,14 @@ import (
 var _ = Describe("RetrieveInvoice", func() {
 	It("Retrieves an invoice from the server with an id", func() {
 		time.Sleep(3 * time.Second)
-		pm := ku.GeneratePem()
+		pm := os.ExpandEnv("$BITPAYPEM")
 		apiuri := os.ExpandEnv("$RCROOTADDRESS")
 		webClient := Client{ApiUri: apiuri, Insecure: true, Pem: pm}
-		gopath := os.ExpandEnv("$GOPATH")
-		tempFolder := gopath + "/temp/"
-		code, err := ioutil.ReadFile(tempFolder + "retrievecode.txt")
-		if err != nil {
-			println(err.Error())
-		}
-		token, err := webClient.PairWithCode(string(code))
-		if err != nil {
-			println(err.Error())
-		}
-		Expect(token.Token).NotTo(BeNil())
-		webClient.Token = token
-		response, err := webClient.CreateInvoice(10, "USD")
-		if err != nil {
-			println("the retrieve test errored while creating an invoice: Error - " + err.Error())
-		}
-		invoiceId := response.Id
+		invoiceId := os.ExpandEnv("$INVOICEID")
 		retrievedInvoice, err := webClient.GetInvoice(invoiceId)
 		if err != nil {
-			println(webClient.ApiUri + " errored retrieving an invoice: Error - " + err.Error())
+			println("\n" + webClient.ApiUri + " errored retrieving an invoice: Error - " + err.Error())
 		}
 		Expect(retrievedInvoice.Id).To(Equal(invoiceId))
-		Expect(retrievedInvoice.Price).To(Equal(response.Price))
 	})
 })
