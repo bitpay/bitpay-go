50b2d62bc06d5ee52c7e385c0ef348e850a14485
paul <paul@gaspode.local>
2015-09-18 15:12:04 -0400

    Added basic Post endpoint
    Fixed travis tests to use environment PEM and existing merchant tokens.

--- client/client_invoice_test.go
+++ client/client_invoice_test.go
@@ -1,8 +1,10 @@
 package client_test
 
 import (
+	"encoding/json"
 	. "github.com/bitpay/bitpay-go/client"
 	ku "github.com/bitpay/bitpay-go/key_utils"
+	"strings"
 
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
@@ -15,18 +17,31 @@ import (
 var _ = Describe("CreateInvoice", func() {
 	It("creates an invoice for the price and currency sent", func() {
 		time.Sleep(3 * time.Second)
-		pm := ku.GeneratePem()
+		pm := os.ExpandEnv("$BITPAYPEM")
+		pm = strings.Replace(pm, "\\n", "\n", -1)
 		apiuri := os.ExpandEnv("$RCROOTADDRESS")
-		webClient := Client{ApiUri: apiuri, Insecure: true, Pem: pm}
-		gopath := os.ExpandEnv("$GOPATH")
-		tempFolder := gopath + "/temp/"
-		code, err := ioutil.ReadFile(tempFolder + "invoicecode.txt")
-		if err != nil {
-			println(err.Error())
+		webClient := Client{ApiUri: apiuri, Insecure: true, Pem: pm, ClientId: ku.GenerateSinFromPem(pm)}
+		mertok, _ := webClient.GetToken("merchant")
+		params := make(map[string]string)
+		params["token"] = mertok
+		params["facade"] = "pos"
+		params["id"] = webClient.ClientId
+		res, err := webClient.Post("tokens", params)
+		defer res.Body.Close()
+		contents, _ := ioutil.ReadAll(res.Body)
+		var jsonContents map[string]interface{}
+		json.Unmarshal(contents, &jsonContents)
+		var tok Token
+		if res.StatusCode/100 != 2 {
+			Expect(res.StatusCode).To(Equal(200))
+		} else {
+			tok, err = processToken(res, jsonContents)
+			err = nil
 		}
+		code := tok.PairingCode
 		token, err := webClient.PairWithCode(string(code))
 		if err != nil {
-			println(err.Error())
+			Expect(err.Error).To(Equal("Should be no error"))
 		}
 		webClient.Token = token
 		response, err := webClient.CreateInvoice(10, "USD")
